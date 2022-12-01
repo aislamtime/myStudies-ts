4b20ffd9045fb8980be63aedc4ec917601627375
aislamtime <116115530+aislamtime@users.noreply.github.com>
2022-12-01 17:13:06 +0300

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -6,45 +6,24 @@ import { Input } from './components/Input';
 import { Filter, MoneyType } from './components/Filter';
 import { CounterMain } from './components/CounterMain/CounterMain';
 
+import { Rating } from './components/Rating/Rating';
+import { Accordion } from './components/Accordion/Accordion';
+import { ValueType } from './components/Rating/Star/Star';
+
 function App() {
-	//const [money, setMoney] = useState([
-	//	{ banknots: 'Dollars', value: 100, number: ' a1234567890' },
-	//	{ banknots: 'Dollars', value: 50, number: ' z1234567890' },
-	//	{ banknots: 'RUBLS', value: 100, number: ' w1234567890' },
-	//	{ banknots: 'Dollars', value: 100, number: ' e1234567890' },
-	//	{ banknots: 'Dollars', value: 50, number: ' c1234567890' },
-	//	{ banknots: 'RUBLS', value: 100, number: ' r1234567890' },
-	//	{ banknots: 'Dollars', value: 50, number: ' x1234567890' },
-	//	{ banknots: 'RUBLS', value: 50, number: ' v1234567890' },
-	//]);
-
-	const [message, setMessage] = useState([
-		{ message: 'message 1' },
-		{ message: 'message 2' },
-		{ message: 'message 3' },
-	]);
-
-	let [title, setTitle] = useState('');
-
-	const addMessage = () => {
-		let newMEssage = setMessage([{ message: title }, ...message]);
-		setTitle('');
-	};
+	const [ratingValue, setRatingValue] = useState<ValueType>(0);
+	const [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false);
 
 	return (
 		<div className='App'>
-			<CounterMain />
+			{/*<CounterMain />*/}
 
-			{/*<Input change={setTitle} title={title} />
-			<Button name='+' callBack={addMessage} />
+			<Accordion
+				collapsed={accordionCollapsed}
+				setCollapsed={setAccordionCollapsed}
+			/>
 
-			<div>
-				{message.map((el, index) => {
-					return <div key={index}>{el.message}</div>;
-				})}
-			</div>*/}
-
-			{/*<Filter money={money} />*/}
+			<Rating value={ratingValue} setValue={setRatingValue} />
 		</div>
 	);
 }
